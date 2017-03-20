bfdf462e35bb17c35642152c1c48296b3f1e7e81
Kevin Hernández Rostrán <kevinah95@gmail.com>
2017-03-20 11:56:46 -0600

    Update interval.py

--- interval.py
+++ interval.py
@@ -1,10 +1,12 @@
+import random,math
+
 def interval(string,start,end):
     return string[start:end]
 
 def concat(fst_string,snd_string):
     return fst_string + snd_string
-def sufix(string,lenght):
-    return string[-abs(lenght):]
+def sufix(string,length):
+    return string[-abs(length):]
 def complement(string):
     string = string.upper()
     d={'A' : 'T', 'T' : 'A',
@@ -15,4 +17,33 @@ def complement_reverse(string):
     string = string[::-1]
     return complement(string)
 
+def sufix_check(string, sufix):
+    length = len(sufix)
+    if(length <= len(string)):
+        to_compare = string[:length]
+        if(to_compare == sufix):
+            print("SÃ­ es sufijo")
+        else:
+            print("No es sufijo")
+    else:
+        print("No es sufijo")
+
+
+def poisson(mu):
+    b = 1
+    i = 0
+    while b >= math.exp(-mu):
+        b *= random.uniform(0, 1)
+        i += 1
+    return i - 1
+
+def normal(mu,sigma):
+    p1 = random.uniform(-1, 1)
+    p2 = random.uniform(-1, 1)
+    p = p1 * p1 + p2 * p2   
+    while (p >= 1):
+        p1 = random.uniform(-1, 1)
+        p2 = random.uniform(-1, 1)
+        p = p1 * p1 + p2 * p2
+    return mu + sigma * p1 * math.sqrt(-2 * math.log(p) / p)
 
