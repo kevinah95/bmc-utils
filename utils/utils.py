c389ced1b76449a2536f1846228f158acceab3dc
dianvargas <diana210496@hotmail.com>
2017-03-20 14:49:00 -0600

    Update utils.py

--- utils/utils.py
+++ utils/utils.py
@@ -34,14 +34,18 @@ def verify_arn(string):
 	    # else:
 	    #     print("No es una secuencia de ARN válida")
 	return flag
+
 ## largo de hilera
+
 def length(string):
     return len(string)
 ## obtener el elemento i-ésimo de una hilera dada
 def i_esimo(string)
 	indice=string[1]
 	print(indice)
+
 ##verificar si s es una subsecuencia de t, además el proceso inverso, si t una supersecuencia de s
+
 def subsecuence(str1,str2):
 str1=str1.upper()
 str2=str2.upper()
@@ -56,7 +60,9 @@ else:
         print("s es subsecuencia de t y t es supersecuencia de s")
     else:
         print("s no es subsecuencia de t y t no es supersecuencia de s")
+
 ## verificar si s es un substring de t, además el proceso inverso, si t una superstring de s
+
 def substring(str1,str2)
 str1=str1.upper()
 str2=str2.upper()
@@ -64,20 +70,30 @@ if(str1 in str2):
     print("s es substring de t y t es super string de s")
 else:
     print("s no es substring de t y t no es super string de s")
+
 ## operación intervalo, recibe índices i y j de entrada
+
 def interval(string,start,end):
     return string[start:end]
+
 ## operación concatenación
+
 def concat(fst_string,snd_string):
     return fst_string + snd_string
+
 ## operación prefijo, recibe índices s y k de entrada, para obtener los primeros k simbolos de s
+
 def prefix(string,length)
 	result=string[:length]
 	print(result)
+
 ## operación sufijo, recibe índices s y k de entrada, para obtener los últimos k simbolos de s
+
 def sufix(string,length):
     return string[-abs(length):]
+
 ## verificar si p es un prefijo de s
+
 def prefix_check(string, prefix):
     length = len(prefix)
     flag = False
@@ -87,7 +103,9 @@ def prefix_check(string, prefix):
             flag = True
     return flag
 print(prefix_check(string,prefix))
+
 ## verificar si s es un sufijo de t
+
 def sufix_check(string, sufix):
     length = len(sufix)
     if(length <= len(string)):
@@ -98,13 +116,17 @@ def sufix_check(string, sufix):
             print("No es sufijo")
     else:
         print("No es sufijo")
+
 ## calcular el complemento de una hilera de ADN
+
 def complement(string):
     string = string.upper()
     d={'A' : 'T', 'T' : 'A',
        'C' : 'G', 'G' : 'C'}
     return ''.join(d[s] for s in string if s in d.keys())
+
 ## calcular el complemento inverso de una hilera de ADN
+
 def complement_reverse(string):
     string = string.upper()
     string = string[::-1]
